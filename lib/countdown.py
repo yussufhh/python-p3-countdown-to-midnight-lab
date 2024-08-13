ade22abfb88a9340e4e78015ba7cbff0513e1a6e
yussufhh <0793477632g@gmail.com>
2024-08-13 11:25:27 +0300

    final-commit

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,14 @@
-# your code goes here!
+import time
+
+def countdown(number):
+    while number >= 1:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(number):
+    while number >= 1:
+        print(f'{number} SECOND(S)!')
+        time.sleep(1)  # Sleep for 1 second
+        number -= 1
+    print("HAPPY NEW YEAR!")
--- lib/debug.py
+++ lib/debug.py
@@ -3,4 +3,5 @@
 from countdown import countdown, countdown_with_sleep
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    import ipdb; ipdb.set_trace()  # Set a breakpoint for debugging
+    countdown(5)  # Call your function or use countdown_with_sleep(5) as needed
